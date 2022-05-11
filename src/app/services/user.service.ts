4340ba93635b120317d8bab9530348deb73a419d
BogdanSydir <taote92@gmail.com>
2022-05-11 12:46:53 +0200

    hw_todo

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {IUser} from "../model/IUser";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getUsers(): Observable<IUser[]> {
+    return this.http.get<any[]>('https://jsonplaceholder.typicode.com/users')
+
+  }
+}
